b4404a030dae22a446801c6a09a493e03b965259
aarondud <aarondudley00@gmail.com>
2023-02-28 15:56:18 +0800

    feat: resetArray function generates new array

--- src/SortingVisualiser/SortingVisualiser.jsx
+++ src/SortingVisualiser/SortingVisualiser.jsx
@@ -9,5 +9,23 @@ export default class SortingVisualiser extends React.Compnent {
             array: [],
         };
     }
+
+    // this is used during generate new array button
+    resetArray() {
+        const array = [];
+        const noElements = 100 // no of elements in an array
+        const MAXVALUE = 10000 // max value of random integer 
+        
+        for (let i = 0; i < noElements; i++) {
+            array.push(this.getRandomInt(MAXVALUE));
+        }
+
+        this.setState({array});
+    }
+
+    // gets random integer for resetting array
+    getRandomInt(max) {
+        return Math.floor(Math.random() * max);
+    };
 }
 
