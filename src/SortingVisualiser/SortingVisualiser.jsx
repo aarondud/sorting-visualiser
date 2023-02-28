96a15462fecc8eccc0ad658bf09bd8307aa0bb1e
aarondud <aarondudley00@gmail.com>
2023-02-28 15:34:54 +0800

    feat: SortingVisualiser component created

---
+++ src/SortingVisualiser/SortingVisualiser.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './SortingVisualiser.css';
+
+export default class SortingVisualiser extends React.Compnent {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            array: [],
+        };
+    }
+}
+
