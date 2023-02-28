b86676af9069e948900742cddcc1a9f1dc3d9687
aarondud <aarondudley00@gmail.com>
2023-02-28 16:23:26 +0800

    feat: render array as numbers

--- src/SortingVisualiser/SortingVisualiser.jsx
+++ src/SortingVisualiser/SortingVisualiser.jsx
@@ -1,31 +1,46 @@
 import React from 'react';
 import './SortingVisualiser.css';
 
-export default class SortingVisualiser extends React.Compnent {
+export default class SortingVisualiser extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {
-            array: [],
-        };
+        this.state = {array: [],};
+    }
+
+    componentDidMount() {
+        this.resetArray();
     }
 
     // this is used during generate new array button
     resetArray() {
         const array = [];
-        const noElements = 100 // no of elements in an array
-        const MAXVALUE = 10000 // max value of random integer 
+        const noElements = 100; // no of elements in an array
+        const MAXVALUE = 10000; // max value of random integer 
         
         for (let i = 0; i < noElements; i++) {
-            array.push(this.getRandomInt(MAXVALUE));
+            array.push(getRandomInt(MAXVALUE));
         }
 
         this.setState({array});
     }
 
-    // gets random integer for resetting array
-    getRandomInt(max) {
-        return Math.floor(Math.random() * max);
-    };
+    render() {
+        const {array} = this.state;
+
+        return (
+            <>
+            {array.map((value, idx) => (
+                <div className="array-bar" key={idx}>
+                {value}
+                </div>
+            ))}
+            </>
+            );
+        }
 }
 
+// gets random integer for resetting array
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+};
