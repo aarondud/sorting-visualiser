6cf390ae412ae2a889c9b5a3b6ac5063722df27e
aarondud <aarondudley00@gmail.com>
2023-02-28 16:31:45 +0800

    feat: render array as bars (css hardcoded)

--- src/SortingVisualiser/SortingVisualiser.jsx
+++ src/SortingVisualiser/SortingVisualiser.jsx
@@ -29,14 +29,15 @@ export default class SortingVisualiser extends React.Component {
         const {array} = this.state;
 
         return (
-            <>
+            <div className="array-container">
             {array.map((value, idx) => (
-                <div className="array-bar" key={idx}>
-                {value}
-                </div>
+                <div
+                className="array-bar" 
+                key={idx}
+                style={{height: `${value}px`}}
+                ></div>
             ))}
-            </>
-            );
+            </div>)
         }
 }
 
