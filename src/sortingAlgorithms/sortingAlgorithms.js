29deb9ccf4b9f4ec1b871dd5da47d9cb985ad2d3
aarondud <aarondudley00@gmail.com>
2023-03-01 11:14:29 +0800

    docs: addded mergeSort() documentation

--- src/sortingAlgorithms/sortingAlgorithms.js
+++ src/sortingAlgorithms/sortingAlgorithms.js
@@ -1,30 +1,51 @@
+/**
+ * Implements Merge Sort in ascending order
+ * Stable, recursive, divide & conquer, O(nlogn)
+ * Works well with large datasets, if size of dataset unknown consider other alogrithm like insertion sort
+
+ * @param {array} array unsorted array of doubles
+ * @returns {array} ascending sorted array
+ */
 function mergeSort(array) {
 
+    // recursion finished when 1 item remaining in array
     if (array.length <= 1) {
         return array;
     }
 
+    // recursively call merge sort, recursively cutting array in half
     let mid = Math.floor(array.length / 2),
         leftArray = mergeSort(array.slice(0,mid)),
         rightArray = mergeSort(array.slice(mid));
     
+    // use merge utility to start build arrays back up, with each merge sorting along the way
     return mergeArray(leftArray, rightArray);
     
 
+    /**
+     * Implements sorting and merging of two arrays
+     * O(N), where N = max(arr1.length, arr2.length)
+     * 
+     * @param {array} arr1 an array
+     * @param {array} arr2 an array
+     * @returns {array} sorted merged array
+     */
     function mergeArray(arr1, arr2) {
-                
-       let sorted = [];
+            
+        let sorted = [];
 
-       while (arr1.length && arr2.length) {
+        //while there are items in either array, check which first item is smaller, add it to the sorted array, and remove it from it's original array
+        while (arr1.length && arr2.length) {
             if (arr1[0] < arr2[0]) {
                 sorted.push(arr1.shift());
             }
             else {
                 sorted.push(arr2.shift());
             }
-       }
+        }
 
-       return sorted.concat(arr1,arr2);
+        // If there's anything left over (ie. arrays not same size) concatonate remaining onto the end
+        return sorted.concat(arr1,arr2);
     }
 
 }
